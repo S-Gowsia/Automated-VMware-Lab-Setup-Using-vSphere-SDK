30af5a6e2a3d6393c588c2d7b526f94308875830
S-Gowsia <shaikgowsia123@gmail.com>
2025-04-06 12:27:07 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        JSONObject config = new JSONObject(Files.readString(Paths.get("config/vm-config.json")));
 
+        ServiceInstance si = VsphereConnector.connect("https://" + config.getString("vcenter") + "/sdk",
+                                                      config.getString("username"),
+                                                      config.getString("password"));
+
+        new VmDeployer(si).deployVMs(config);
+        new SnapshotManager(si).createSnapshot(config);
+        new PowerManager(si).powerAction(config, "on");
+
+        si.getServerConnection().logout();
+    }
+}
